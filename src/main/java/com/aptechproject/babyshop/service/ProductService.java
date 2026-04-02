aa0c570c6c2ac288949eaafaf42cc23b935d503c
Nnanna Osinachukwu <contact2amondz24@gmail.com>
2026-04-02 20:09:51 +0100

    feat(repository, service): Repository and Service for Product logic
    - `ProductRepository` and `ProductService`
    - minimal setup of Repository, simply extending with `JpaRepository` and providing <Object, Id> -> `<Product, Long>`
    - Dependency Injection of `ProductRepository` in `ProductService` via constructor
    - 2 methods `addProduct()` and `getAllProducts()

---
+++ src/main/java/com/aptechproject/babyshop/service/ProductService.java
@@ -0,0 +1,28 @@
+package com.aptechproject.babyshop.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.aptechproject.babyshop.model.Product;
+import com.aptechproject.babyshop.repository.ProductRepository;
+
+@Service
+public class ProductService {
+    
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product addProduct(Product product) {
+        productRepository.save(product);
+        return product;
+    }
+
+    public List<Product> getAllProducts() {
+        List<Product> allProducts = productRepository.findAll();
+        return allProducts;
+    }
+}
