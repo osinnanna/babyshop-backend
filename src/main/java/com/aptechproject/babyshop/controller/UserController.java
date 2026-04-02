ce17c998373420e37a9e909359506e57b9b785f5
Nnanna Osinachukwu <contact2amondz24@gmail.com>
2026-04-02 14:59:09 +0100

    feat(controller): UserController register /register endpoint
    - main `UserController` endpoint `/api/users`
    - Dependency injection of userService
    - `registerUser()` function for "/register" endpoint
    - implemented try_catch with appropriate Responses. 2 which are `CREATED(201)` and `BAD_REQUEST(400)`

---
+++ src/main/java/com/aptechproject/babyshop/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.aptechproject.babyshop.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.aptechproject.babyshop.model.User;
+import com.aptechproject.babyshop.service.UserService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody User newUser) {
+        // using @Valid checks the acutal validations set in the User.java file
+
+        // 1. Call UserService to register User
+        // 2. Return upon success a response containing the user
+        // 3. If user already exists return it and repond to said situation
+        try {
+            userService.registerUser(newUser);
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(newUser.getEmail());
+        } catch (RuntimeException e) { // The error that was thrown in UserService.registerUser()
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+}
