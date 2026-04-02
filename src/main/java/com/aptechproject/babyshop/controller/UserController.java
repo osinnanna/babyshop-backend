3adf9ea93abdfec4545070246912b23eb49fe80b
Nnanna Osinachukwu <contact2amondz24@gmail.com>
2026-04-02 17:41:29 +0100

    feat(controller): loginUser implementaion in `UserController`
    - implements `UserService.loginUser()` method
    - appropriate success and failure responses
    - yet to return `jwt`

--- src/main/java/com/aptechproject/babyshop/controller/UserController.java
+++ src/main/java/com/aptechproject/babyshop/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.aptechproject.babyshop.constant.AppConstants;
+import com.aptechproject.babyshop.dto.LoginRequest;
 import com.aptechproject.babyshop.model.User;
 import com.aptechproject.babyshop.service.UserService;
 
@@ -35,6 +36,22 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.CREATED).body(newUser.getEmail());
         } catch (RuntimeException e) { // The error that was thrown in UserService.registerUser()
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @PostMapping(AppConstants.API_LOGIN)
+    public ResponseEntity<?> loginUser(@Valid @RequestBody LoginRequest loginRequest) {
+        // 1. Using try catch to implement the already made method in UserService
+        // 2. In try use the email and password in LoginRequest to login
+        // 3. If success return appropriate response
+        // 4. In catch, the thrown RuntimeException should be handled properly
+
+        // 1
+        try {
+            userService.loginUser(loginRequest.getEmail(), loginRequest.getPassword());
+            return ResponseEntity.status(HttpStatus.OK).body(loginRequest.getEmail());
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
         }
     }
 }
