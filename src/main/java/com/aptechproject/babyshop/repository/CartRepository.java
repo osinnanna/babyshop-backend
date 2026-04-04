9c1079bf871f6f785309f9677a13daef7fa8614c
Nnanna Osinachukwu <contact2amondz24@gmail.com>
2026-04-04 14:16:09 +0100

    feat(repository): add method to find `Cart` by `User`

--- src/main/java/com/aptechproject/babyshop/repository/CartRepository.java
+++ src/main/java/com/aptechproject/babyshop/repository/CartRepository.java
@@ -1,8 +1,12 @@
 package com.aptechproject.babyshop.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.aptechproject.babyshop.model.Cart;
+import com.aptechproject.babyshop.model.User;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
+    Optional<Cart> findByUser(User user);
 }
